f8c230cee1b3b537e25532d02597941dd3d21cf8
Ammar Multani <ammarmultani007@gmail.com>
2024-08-11 19:06:11 +0530

    feature 3.0.1
    dark mode

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+// import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 function App() {
@@ -8,6 +9,8 @@ function App() {
     <Navbar title="TextUtils" />
     <div className="container my-4  ">
     <TextForm heading = "Enter the text to convert"/>
+
+    {/* <About/> */}
     </div>
     </>
   );
