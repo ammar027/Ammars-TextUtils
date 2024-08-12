22bcf4de2941da90e584f9bef4b77aa464b5003c
Ammar Multani <ammarmultani007@gmail.com>
2024-08-12 22:17:55 +0530

    Fixes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 export default function Navbar({ title = '', aboutText = 'About', mode = 'light', theme = 'light', setTheme, toggleMode }) {
   // Get the text for the dropdown button based on the current theme
@@ -20,17 +20,17 @@ export default function Navbar({ title = '', aboutText = 'About', mode = 'light'
   return (
     <nav className={`navbar navbar-expand-lg navbar-${mode} bg-${mode}`}>
       <div className="container-fluid">
-        <Link className="navbar-brand" to="/">{title}</Link>
+        <NavLink className="navbar-brand" to="/">{title}</NavLink>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link to="/" className="nav-link active" aria-current="page">Home</Link>
+              <NavLink exact to="/" className="nav-link" activeClassName="active" aria-current="page">Home</NavLink>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/about">{aboutText}</Link>
+              <NavLink to="/about" className="nav-link" activeClassName="active">{aboutText}</NavLink>
             </li>
           </ul>
           <div className="d-flex align-items-center">
