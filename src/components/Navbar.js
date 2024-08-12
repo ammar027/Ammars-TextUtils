8c194a6bf7b65fa22e08b441edfbc77b7df7aad7
Ammar Multani <ammarmultani007@gmail.com>
2024-08-12 18:54:42 +0530

    Final Build

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 export default function Navbar({ title = 'Set title here', aboutText = 'About', mode = 'light', theme = 'light', setTheme, toggleMode }) {
@@ -9,9 +9,9 @@ export default function Navbar({ title = 'Set title here', aboutText = 'About', 
       case 'dark':
         return 'Dark';
       case 'dark-blue':
-        return 'Dark Blue';
+        return 'Nightly Blue';
       case 'dark-green':
-        return 'Dark Green';
+        return 'Nightly Green';
       default:
         return 'Theme';
     }
@@ -27,7 +27,7 @@ export default function Navbar({ title = 'Set title here', aboutText = 'About', 
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link to="/" className="nav-link active" aria-current="page" >Home</Link>
+              <Link to="/" className="nav-link active" aria-current="page">Home</Link>
             </li>
             <li className="nav-item">
               <Link className="nav-link" to="/about">{aboutText}</Link>
@@ -35,20 +35,26 @@ export default function Navbar({ title = 'Set title here', aboutText = 'About', 
           </ul>
           <div className="d-flex align-items-center">
             {mode === 'dark' && (
-              <div className="nav-item dropdown me-3">
+              <div className="nav-item dropdown me-2">
                 <button className={`btn btn-${mode} dropdown-toggle`} data-bs-toggle="dropdown" aria-expanded="false">
                   {getDropdownText()}
                 </button>
                 <ul className={`dropdown-menu dropdown-menu-${mode}`}>
                   <li><button className="dropdown-item" onClick={() => setTheme('dark')}>Dark</button></li>
-                  <li><button className="dropdown-item" onClick={() => setTheme('dark-blue')}>Dark Blue</button></li>
-                  <li><button className="dropdown-item" onClick={() => setTheme('dark-green')}>Dark Green</button></li>
+                  <li><button className="dropdown-item" onClick={() => setTheme('dark-blue')}>Nightly Blue</button></li>
+                  <li><button className="dropdown-item" onClick={() => setTheme('dark-green')}>Nightly Green</button></li>
                 </ul>
               </div>
             )}
-            <div className={`form-check form-switch text-${mode === 'light' ? 'dark' : 'light'} ms-3`}>
-              <input className="form-check-input" onClick={toggleMode} type="checkbox" role="switch" id="flexSwitchCheckDefault" />
-              <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Dark Mode</label>
+            <div className={`form-check form-switch ms-1 mx-3 text-${mode === 'light' ? 'dark' : 'light'}`}>
+              <input className="form-check-input" onClick={toggleMode} type="checkbox" role="switch" id="flexSwitchCheckDefault" checked={mode === 'dark'} />
+              <label className="form-check-label d-flex mx-1 align-items-center" htmlFor="flexSwitchCheckDefault">
+                {mode === 'light' ? (
+                  <i className="bi bi-sun"></i> // Light mode icon
+                ) : (
+                  <i className="bi bi-moon"></i> // Dark mode icon
+                )}
+              </label>
             </div>
           </div>
         </div>
